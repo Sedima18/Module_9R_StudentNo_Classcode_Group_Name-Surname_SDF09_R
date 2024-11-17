b6dc6fad5cb699ffb42dae17f185ac6dde9ee48e
Sedima Mohale <sedimamohale18@gmail.com>
2024-11-17 19:55:21 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,100 @@
-// 1. Create two variables, firstCard and secondCard. 
-// Set their values to a random number between 2-11
+let cards = [];
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = 0;
 
-let firstCard = 6
-let secondCard = 9
+let hasBlackjack = false;
 
-let sum = firstCard + secondCard
+let isAlive = false;
 
-console.log(sum)
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+
+let sumEl = document.getElementById("sum-el");
+
+let cardsEl = document.getElementById("cards-el");
+
+let startEl = document.getElementById("start");
+
+let newEl = document.getElementById("new");
+
+let playerName = "Player";
+
+let playerChip = Math.floor(Math.random() * 51);
+
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = `${playerName}: $${playerChip}`;
+
+function getRandomCard() {
+ let randomNumbers = Math.floor(Math.random() * 13) + 1;
+ if (randomNumbers > 10) {
+  return 10;
+ } else if (randomNumbers === 1){
+  return 11;
+ } else{
+  return randomNumbers;
+ }
+}
+
+startEl.addEventListener('click', () => {
+ isAlive = true;
+ let firstCard = getRandomCard();
+ let secondCard = getRandomCard();
+ cards = [firstCard, secondCard];
+ sum = firstCard + secondCard;
+ renderGame();
+ document.querySelector("#start").style.display = "none";
+})
+
+const resetGame = () => {
+    cards = [];
+    sum = 0;
+    hasBlackjack = false;
+    isAlive = false;
+    message = "";
+    renderGame();
+    document.querySelector("#start").style.display = "flex";
+};
+
+const renderGame = () => {
+    cardsEl.textContent = `Cards: `;
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += `${cards[i]} `;
+        if (i < cards.length - 1) {
+            cardsEl.textContent += '+ ';
+        }
+    }
+    sumEl.textContent = `Sum: ${sum}`;
+    if (sum < 21) {
+        message = "Do you want to draw a new card?";
+        isAlive = true;
+    } else if (sum === 21) {
+        message = "Whoo! You've got BlackJack!";
+        hasBlackjack = true;
+        isAlive = true;
+        setTimeout(() => {
+            if (confirm('Play Again?')) {
+                resetGame();
+            }
+        }, 1000);
+    } else {
+        message = "You're out of the Game!";
+        isAlive = false;
+        setTimeout(() => {
+            if (confirm('Play Again?')) {
+                resetGame();
+            }
+        }, 1000);
+    }
+    messageEl.textContent = message;
+};
+
+newEl.addEventListener('click', () => {
+ if(isAlive === true && hasBlackjack === false) {
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+  renderGame();
+ }
+});
